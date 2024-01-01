ce0835caafc45c3118658ebef21b784733ed82a6
unknown <davedalimocon@gmail.com>
2024-01-01 17:56:06 +0800

    github login

---
+++ src/main/java/com/example/usadpa/UsadPaApplication.java
@@ -0,0 +1,13 @@
+package com.example.usadpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsadPaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsadPaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/usadpa/UsadPaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usadpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsadPaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
